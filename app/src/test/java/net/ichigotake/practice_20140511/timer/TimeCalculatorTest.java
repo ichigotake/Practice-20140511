49fb90ae97728d3734f2ddda3ead929c78de0ec8
ichigotake <ichigotake.san@gmail.com>
2015-05-11 06:11:03 +0900

    Fix typo

--- app/src/test/java/net/ichigotake/practice_20140511/timer/TimeCalculatorTest.java
+++ app/src/test/java/net/ichigotake/practice_20140511/timer/TimeCalculatorTest.java
@@ -15,19 +15,19 @@ import static junit.framework.Assert.assertTrue;
 public class TimeCalculatorTest {
 
     @Test
-    public void test_hello() {
-        assertTrue("(startHour < periodHour) && (startHour == target < periodHour)", between(2, 10, 2));
-        assertTrue("(startHour < periodHour) && (startHour < target < periodHour)", between(2, 10, 9));
-        assertFalse("(startHour < periodHour) && (startHour < target == periodHour)", between(2, 10, 10));
-        assertFalse("(startHour < periodHour) && (startHour < target > periodHour)", between(2, 10, 20));
+    public void test_TimeCalculator_isBetween() {
+        assertTrue("(startHour < periodHour) && (startHour == betweenHour < periodHour)", isBetween(2, 10, 2));
+        assertTrue("(startHour < periodHour) && (startHour < betweenHour < periodHour)", isBetween(2, 10, 9));
+        assertFalse("(startHour < periodHour) && (startHour < betweenHour == periodHour)", isBetween(2, 10, 10));
+        assertFalse("(startHour < periodHour) && (startHour < betweenHour > periodHour)", isBetween(2, 10, 20));
 
-        assertTrue("(startHour > periodHour) && (startHour == target < periodHour)", between(16, 10, 16));
-        assertTrue("(startHour > periodHour) && (startHour < target < periodHour)", between(16, 10, 9));
-        assertFalse("(startHour > periodHour) && (startHour < target == periodHour)", between(16, 10, 10));
-        assertFalse("(startHour > periodHour) && (startHour < target > periodHour)", between(16, 10, 14));
+        assertTrue("(startHour > periodHour) && (startHour == betweenHour < periodHour)", isBetween(16, 10, 16));
+        assertTrue("(startHour > periodHour) && (startHour < betweenHour < periodHour)", isBetween(16, 10, 9));
+        assertFalse("(startHour > periodHour) && (startHour < betweenHour == periodHour)", isBetween(16, 10, 10));
+        assertFalse("(startHour > periodHour) && (startHour < betweenHour > periodHour)", isBetween(16, 10, 14));
     }
 
-    private boolean between(int start, int end, int betweenHour) {
+    private boolean isBetween(int start, int end, int betweenHour) {
         return new TimeCalculator(new Timer(start, end)).isBetween(betweenHour);
     }
 }
